25a37af743f357f1b6edd631a55e063b0bba36db
Ross MacArthur <ross@macarthur.io>
2020-07-07 18:26:33 +0200

    Add USB HID descriptor

--- spoofer/src/usb_hid.rs
+++ spoofer/src/usb_hid.rs
@@ -2,7 +2,10 @@
 //!
 //! Some useful links:
 //! - https://www.usb.org/sites/default/files/hid1_11.pdf
+//! - https://www.usb.org/document-library/hid-usage-tables-112
 //! - https://gist.github.com/ToadKing/b883a8ccfa26adcc6ba9905e75aeb4f2
+//! - https://github.com/agalakhov/usbd-hid-device
+//! - https://github.com/agalakhov/usbd-hid-device-example
 
 use core::cell::RefCell;
 
@@ -10,6 +13,101 @@ use cortex_m::interrupt::{free, Mutex};
 use stm32f4xx_hal::otg_fs::UsbBusType;
 use usb_device::class_prelude::*;
 use usb_device::prelude::*;
+
+#[rustfmt::skip]
+const HID_DESCRIPTOR: &[u8] = &[
+    0x05, 0x01,                   // Usage Page (Generic Desktop Ctrls)
+    0x15, 0x00,                   // Logical Minimum (0)
+    0x09, 0x04,                   // Usage (Joystick)
+    0xA1, 0x01,                   // Collection (Application)
+    0x85, 0x30,                   //   Report ID (48)
+    0x05, 0x01,                   //   Usage Page (Generic Desktop Ctrls)
+    0x05, 0x09,                   //   Usage Page (Button)
+    0x19, 0x01,                   //   Usage Minimum (0x01)
+    0x29, 0x0A,                   //   Usage Maximum (0x0A)
+    0x15, 0x00,                   //   Logical Minimum (0)
+    0x25, 0x01,                   //   Logical Maximum (1)
+    0x75, 0x01,                   //   Report Size (1)
+    0x95, 0x0A,                   //   Report Count (10)
+    0x55, 0x00,                   //   Unit Exponent (0)
+    0x65, 0x00,                   //   Unit (None)
+    0x81, 0x02,                   //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x05, 0x09,                   //   Usage Page (Button)
+    0x19, 0x0B,                   //   Usage Minimum (0x0B)
+    0x29, 0x0E,                   //   Usage Maximum (0x0E)
+    0x15, 0x00,                   //   Logical Minimum (0)
+    0x25, 0x01,                   //   Logical Maximum (1)
+    0x75, 0x01,                   //   Report Size (1)
+    0x95, 0x04,                   //   Report Count (4)
+    0x81, 0x02,                   //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x75, 0x01,                   //   Report Size (1)
+    0x95, 0x02,                   //   Report Count (2)
+    0x81, 0x03,                   //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x0B, 0x01, 0x00, 0x01, 0x00, //   Usage (0x010001)
+    0xA1, 0x00,                   //   Collection (Physical)
+    0x0B, 0x30, 0x00, 0x01, 0x00, //     Usage (0x010030)
+    0x0B, 0x31, 0x00, 0x01, 0x00, //     Usage (0x010031)
+    0x0B, 0x32, 0x00, 0x01, 0x00, //     Usage (0x010032)
+    0x0B, 0x35, 0x00, 0x01, 0x00, //     Usage (0x010035)
+    0x15, 0x00,                   //     Logical Minimum (0)
+    0x27, 0xFF, 0xFF, 0x00, 0x00, //     Logical Maximum (65534)
+    0x75, 0x10,                   //     Report Size (16)
+    0x95, 0x04,                   //     Report Count (4)
+    0x81, 0x02,                   //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0xC0,                         //   End Collection
+    0x0B, 0x39, 0x00, 0x01, 0x00, //   Usage (0x010039)
+    0x15, 0x00,                   //   Logical Minimum (0)
+    0x25, 0x07,                   //   Logical Maximum (7)
+    0x35, 0x00,                   //   Physical Minimum (0)
+    0x46, 0x3B, 0x01,             //   Physical Maximum (315)
+    0x65, 0x14,                   //   Unit (System: English Rotation, Length: Centimeter)
+    0x75, 0x04,                   //   Report Size (4)
+    0x95, 0x01,                   //   Report Count (1)
+    0x81, 0x02,                   //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x05, 0x09,                   //   Usage Page (Button)
+    0x19, 0x0F,                   //   Usage Minimum (0x0F)
+    0x29, 0x12,                   //   Usage Maximum (0x12)
+    0x15, 0x00,                   //   Logical Minimum (0)
+    0x25, 0x01,                   //   Logical Maximum (1)
+    0x75, 0x01,                   //   Report Size (1)
+    0x95, 0x04,                   //   Report Count (4)
+    0x81, 0x02,                   //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x75, 0x08,                   //   Report Size (8)
+    0x95, 0x34,                   //   Report Count (52)
+    0x81, 0x03,                   //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x06, 0x00, 0xFF,             //   Usage Page (Vendor Defined 0xFF00)
+    0x85, 0x21,                   //   Report ID (33)
+    0x09, 0x01,                   //   Usage (0x01)
+    0x75, 0x08,                   //   Report Size (8)
+    0x95, 0x3F,                   //   Report Count (63)
+    0x81, 0x03,                   //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x85, 0x81,                   //   Report ID (-127)
+    0x09, 0x02,                   //   Usage (0x02)
+    0x75, 0x08,                   //   Report Size (8)
+    0x95, 0x3F,                   //   Report Count (63)
+    0x81, 0x03,                   //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x85, 0x01,                   //   Report ID (1)
+    0x09, 0x03,                   //   Usage (0x03)
+    0x75, 0x08,                   //   Report Size (8)
+    0x95, 0x3F,                   //   Report Count (63)
+    0x91, 0x83,                   //   Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Volatile)
+    0x85, 0x10,                   //   Report ID (16)
+    0x09, 0x04,                   //   Usage (0x04)
+    0x75, 0x08,                   //   Report Size (8)
+    0x95, 0x3F,                   //   Report Count (63)
+    0x91, 0x83,                   //   Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Volatile)
+    0x85, 0x80,                   //   Report ID (-128)
+    0x09, 0x05,                   //   Usage (0x05)
+    0x75, 0x08,                   //   Report Size (8)
+    0x95, 0x3F,                   //   Report Count (63)
+    0x91, 0x83,                   //   Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Volatile)
+    0x85, 0x82,                   //   Report ID (-126)
+    0x09, 0x06,                   //   Usage (0x06)
+    0x75, 0x08,                   //   Report Size (8)
+    0x95, 0x3F,                   //   Report Count (63)
+    0x91, 0x83,                   //   Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Volatile)
+    0xC0,                         // End Collection
+];
 
 // A fake USB HID report.
 pub struct Report([u8; 64]);
@@ -86,6 +184,7 @@ where
             0x00, // bInterfaceSubClass
             0x00, // bInterfaceProtocol
         )?;
+        #[rustfmt::skip]
         writer.write(
             0x21, // bDescriptorType, Human Interface Device (HID)
             &[
@@ -93,7 +192,7 @@ where
                 0x00, // bCountryCode
                 0x01, // bNumDescriptors
                 0x22, // bDescriptorType, HID Report
-                0x40, 0x00, // wMaxPacketSize, 64 bytes
+                HID_DESCRIPTOR.len() as u8, (HID_DESCRIPTOR.len() >> 8) as u8, // wDescriptorLength
             ],
         )?;
         writer.endpoint(&self.endpoint)?;
@@ -117,7 +216,7 @@ where
                 let (desc_ty, desc_index) = request.descriptor_type_index();
                 // Report type is 0x22
                 if desc_ty == 0x22 && desc_index == 0 {
-                    xfer.accept_with_static(REPORT.as_ref()).ok();
+                    xfer.accept_with_static(HID_DESCRIPTOR).ok();
                 }
             }
         }
